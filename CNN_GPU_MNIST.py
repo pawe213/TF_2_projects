3d0a96317be19a4c7ff10eb4519d0fd94c8394c3
pawe213 <pawe213@gmail.com>
2021-05-31 23:03:19 +0200

    build model and evaluate

--- CNN_GPU_MNIST.py
+++ CNN_GPU_MNIST.py
@@ -21,3 +21,32 @@ mnist_dataset = tf.keras.datasets.mnist
 scaled_train_images, scaled_test_images = train_images/255.0, test_images/255.0
 scaled_train_images, scaled_test_images = scaled_train_images[..., np.newaxis], scaled_test_images[..., np.newaxis]
 
+# build model
+input_shape = scaled_train_images[0].shape
+
+model = tf.keras.Sequential([
+    Conv2D(8, (3,3), input_shape=input_shape, activation='relu', padding='same'),
+    MaxPooling2D((2,2)),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+# %%
+history  = model.fit(scaled_train_images, train_labels, epochs=5)
+# %%
+frame = pd.DataFrame(history.history)
+acc_plot = frame.plot(y="acc", title="Accuracy vs Epochs", legend=False)
+acc_plot.set(xlabel="Epochs", ylabel="Accuracy")
+
+acc_plot = frame.plot(y="loss", title = "Loss vs Epochs",legend=False)
+acc_plot.set(xlabel="Epochs", ylabel="Loss")
+
+# %%
+
+test = model.evaluate(scaled_test_images, test_labels)
+test_loss, test_accuracy = test
+print(f"Test loss: {test_loss}")
+print(f"Test accuracy: {test_accuracy}")
+# %%
